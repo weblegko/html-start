5f013d7d19b671a24bf8ad024d91658ae392e43f
Dmitry Bednyuk <weblegko@gmail.com>
2022-07-02 19:08:40 +0300

    убрал nav-bar и menu

--- _dev/src/js/main.js
+++ _dev/src/js/main.js
@@ -81,9 +81,20 @@ class Application {
     }
 
 
-    // кастыльное решение 100vh для мобильников
+    // 100vh для мобильников
     initMobileViewPortHeight() {
+
         // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+        /*
+        Прмиер как использовать эту фичу в стилях
+        .module {
+            height: 100vh; //Use vh as a fallback for browsers that do not support Custom Properties
+            height: calc(var(--vh, 1vh) * 100);
+            margin: 0 auto;
+            max-width: 30%;
+        }
+        */
+
         // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
         let vh = window.innerHeight * 0.01;
         // Then we set the value in the --vh custom property to the root of the document
