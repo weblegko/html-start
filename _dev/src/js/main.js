3b6d9392a032ee7d4e37d31f0176d031a91c41c7
Dmitry Bednyuk <weblegko@gmail.com>
2022-07-02 18:11:20 +0300

    wip

--- _dev/src/js/main.js
+++ _dev/src/js/main.js
@@ -1,86 +1,21 @@
-// import "matchmedia-polyfill";
-// import "matchmedia-polyfill/matchMedia.addListener";
-
-//import objectFitImages from "object-fit-images"; // Полифил
-import "lazysizes";
-
-//import {MDCRipple} from '@material/ripple';
-// import {MDCTextField} from '@material/textfield';
-
-import Lightbox from "./components/lightbox";
 import Slider from "./components/slider";
-import ScrollToTopBtn from "./components/scrolltotop-btn";
-import MainMenu from './components/main-menu';
-import NavBar from "./components/nav-bar";
-import ymaps from 'ymaps';
+import Lightbox from "./components/lightbox";
 
 class Application {
 
+
     constructor() {
         this.initCommon();
-        //this.initMaterialDesignStuff();
         this.initSliders();
-        //this.initAjaxBlogLoad();
         this.initMobileViewPortHeight();
-        this.initYandexMap();
     }
 
-
-    // initMaterialDesignStuff() {
-    //     // еффект ripple на кнопках        
-    //     [].map.call(document.querySelectorAll('.mdc-button'), function(el) {
-    //         return new MDCRipple(el);
-    //     });
-
-    //     // текстовые поля форм
-    //     // [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
-    //     //     return new MDCTextField(el);
-    //     // });
-    // }
-    
     
     // Инициализации
     initCommon() {
 
-        //new Lightbox();
-
-        new ScrollToTopBtn();
-
-        new NavBar({offsetForFixed: 300});
-
-        new MainMenu('ul.sf-menu');
-
-        /* Нажали на кнопочку отправки формы обьратной связи*/
-        $('#zayavka-button').click(function(event) {
-            event.preventDefault();
-            $('#zayvka-submit').trigger('click');
-        });
-
-        if( (history.length == 0) && !document.referrer ) {
-            $('.js-go-back').hide();
-        }  
-
-        // прокрутить вниз первый экран
-        $('.js-go-back').click(function (e) {
-            e.preventDefault();
-            if( (1 < history.length) && document.referrer ) {
-                history.back();
-            }
-        });
-
-        //objectFitImages - полифил активация
-        // if (typeof objectFitImages === 'function') {
-        //     objectFitImages($('.image-cover img'));
-        // }
-        
-        // прокрутить вниз первый экран
-        $('.js-go-down').click(function (e) {
-            e.preventDefault();
-            let y = $('.header-wrap').height();
-            $("html, body").animate(
-                { scrollTop: y }, 1000);
-        });
-
+        new Lightbox();
+       
         // // Плавная прокрутка при переходе по якорю
         // const $root = $('html, body');
         // $('#main-menu a').click(function() {
@@ -97,138 +32,9 @@ class Application {
         
     }
 
-
-    initYandexMap() {
-
-        ymaps.load('https://api-maps.yandex.ru/2.1/?lang=ru_RU')
-            .then(maps => {
-                
-                // Создание карты.
-                // https://tech.yandex.ru/maps/doc/jsapi/2.1/dg/concepts/map-docpage/
-                const myMap = new maps.Map('yandex-map', {
-                    center: [51.660781, 39.200269],
-                    zoom: 13,
-                });
-
-                // myMap.controls // добавим всяких кнопок, в скобках их позиции в блоке
-                //         .add('zoomControl', { left: 5, top: 5 }) //Масштаб
-                //         .add('typeSelector') //Список типов карты
-                //         .add('mapTools', { left: 35, top: 5 }) // Стандартный набор кнопок
-                //         .add('searchControl'); // Строка с поиском
-
-                // Добавление метки
-                // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark-docpage/
-                
-
-                /* Создаем кастомные метки */
-                const myPlacemark0 = new maps.Placemark([51.660781, 39.200269], { 
-                        // Хинт показывается при наведении мышкой на иконку метки.
-                        hintContent: 'Содержимое всплывающей подсказки',
-                        // Балун откроется при клике по метке.
-                        balloonContent: '<div class="ballon">Содержимое балуна</div>', // сдесь содержимое балуна в формате html, все стили в css
-                        
-                        iconImageHref: '/img/marker.svg', // картинка иконки
-                        iconImageSize: [40, 40], // размер иконки
-                        iconImageOffset: [-32, -64], // позиция иконки
-                    });
-                    
-                    // тоже самое для других меток
-
-        
-                // После того как метка была создана, добавляем её на карту.
-                myMap.geoObjects.add(myPlacemark0);
-
-
-                // Фикс кривого выравнивания кастомных балунов
-                myMap.geoObjects.events.add([
-                    'balloonopen'
-                ], function (e) {
-                    var geoObject = e.get('target');
-                    myMap.panTo(geoObject.geometry.getCoordinates(), { delay: 0 });
-                });
-
-            })
-            .catch(error => console.log('Не удалось загрузить карту', error));
-
-    }
-
-
-     
+    
     // Инициализация всех слайдеров
     initSliders() {
-
-        // Home Slider
-        let $homeSlider = $('.home-slider');
-        if ($homeSlider.length !== 0) {
-            new Slider($homeSlider, {
-                counter: false,
-                infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                adaptiveHeight: false,
-                fade: true,
-                arrows: true,
-                //dots: true,
-                autoplay: true,
-                //autoplaySpeed: 2000,
-
-                responsive: [
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 1,
-                        }
-                    },
-
-                    {
-                        breakpoint: 766,
-                        settings: {
-                            slidesToShow: 1,
-                            arrows: false
-                        }
-                    },
-                ]
-
-
-            });
-        }
-
-
-        // Post carousel (home page)
-        let $postCarousel = $('.post-carousel');
-        if ($postCarousel.length !== 0) {
-            new Slider($postCarousel, {
-                counter: false,
-                infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                adaptiveHeight: false,
-                fade: false,
-                arrows: true,
-                //dots: true,
-                autoplay: true,
-                //autoplaySpeed: 2000,
-                 
-                responsive: [
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2,
-                        }
-                    },
-
-                    {
-                        breakpoint: 766,
-                        settings: {
-                            slidesToShow: 2,
-                            arrows: false
-                        }
-                    },
-                ]
-            });
-        }
-
-        
 
         // Slider in content
         let $slider = $('.slider');
@@ -242,8 +48,7 @@ class Application {
                              
             });
         }
-        
-        
+
         // Carousel in content
         let $carousel = $('.carousel');
         if ($carousel.length !== 0) {
@@ -269,10 +74,11 @@ class Application {
                 ]
             });
         }
+
     }
 
 
-     // кастыльное решение 100vh для мобильников
+    // кастыльное решение 100vh для мобильников
     initMobileViewPortHeight() {
         // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
         // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
